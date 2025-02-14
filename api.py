598cc32c0e48e55115ce8204d3d85fe0f23e0c4f
Naga <ml1234@columbia.edu>
2025-02-14 16:31:17 -0500

    new stuff

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+import pandas as pd 
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/data", methods=["GET"])
+def data_mean():
+    df = pd.read_csv("energy.csv", encoding='latin1')
+    mean_value = df["Total Interns"].mean()
+    return jsonify({"The mean of interns": mean_value})
+
+if __name__ == "__main__":
+    app.run(debug=True)
