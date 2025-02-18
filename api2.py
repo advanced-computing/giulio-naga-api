ddbb3263d520a2eb24d2ae9a9387bb8c96eff65a
Naga <ml1234@columbia.edu>
2025-02-18 00:56:26 -0500

    update api

---
+++ api2.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, render_template, request
+import pandas as pd 
+
+app = Flask(__name__)
+
+df = pd.read_csv("energy.csv")
+
+@app.route("/")
+def hello_energy():
+    return "<p>Let's explore energy companies!</p>"
+
+@app.route("/intern")
+def intern_mean():
+    intern_mean = df["Total Interns"].mean()
+    intern_min = df["Total Interns"].min()
+    intern_max = df["Total Interns"].max()
+
+    return jsonify(
+        {"The mean of interns": intern_mean,
+        "The min of interns": intern_min,
+        "The max of interns": intern_max}
+    )
+
+@app.route("/employee")
+def employee_mean():
+    employee_mean = df["Employees"].mean()
+    employee_min = df["Employees"].min()
+    employee_max = df["Employees"].max()
+
+    return jsonify(
+        {"The mean of employees": employee_mean,
+        "The min of employees": employee_min,
+        "The max of employees": employee_max}
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
